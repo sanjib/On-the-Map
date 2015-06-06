0f2093268529ea958d65fcdd283bee0d8b6462f2
Sanjib Ahmad <sanjib@sanjib.org>
2015-06-06 07:10:54 +0600

    Add assets and LoginViewController

---
+++ On The Map/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  On The Map
+//
+//  Created by Sanjib Ahmad on 6/6/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
