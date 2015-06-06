4574e0a1a7f83bd41fb0f8b759bb39622d0ee8de
Sanjib Ahmad <sanjib@sanjib.org>
2015-06-06 07:52:33 +0600

    Add custom view for Round Style Buttons

---
+++ On The Map/Views/RoundStyleButton.swift
@@ -0,0 +1,17 @@
+//
+//  RoundStyleButton.swift
+//  On The Map
+//
+//  Created by Sanjib Ahmad on 6/6/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class RoundStyleButton: UIButton {
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5.0
+    }
+
+}
