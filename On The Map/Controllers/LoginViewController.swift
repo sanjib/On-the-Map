4b6aa10069537fba84b12247ff77d9edeef5f289
Sanjib Ahmad <sanjib@sanjib.org>
2015-06-06 09:39:57 +0600

    Create IBAction and IBOutlet for Login storyboard elements

--- On The Map/Controllers/LoginViewController.swift
+++ On The Map/Controllers/LoginViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func loginUdacity(sender: UIButton) {
+        
+    }
     
+    @IBAction func loginFacebook(sender: UIButton) {
+        
+    }
+    
+    @IBAction func signupUdacity(sender: UIButton) {
+        
+    }
 
 }
